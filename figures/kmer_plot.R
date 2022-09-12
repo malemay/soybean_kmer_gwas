af6efefb0b30aa346ac43ff0bb03d3366b995404
Marc-André Lemay <marc-andre.lemay.2@ulaval.ca>
2022-09-12 11:21:05 -0400

    Move the functions in figures/kmer_plot.R to the gwastools package

--- figures/kmer_plot.R
+++ figures/kmer_plot.R
@@ -3,24 +3,16 @@ library(grid)
 library(Biostrings)
 library(IRanges)
 library(stringr)
+library(gwastools)
 
-max_kmers <- 10000
-kmer_length <- 31
+# Reading the locus from the command line
+locus <- commandArgs(trailingOnly = TRUE)[1]
 
-# A function that maps a set of numeric values onto a color palette
-# Values at the minimum are set to black
-map_color <- function(values, max, min = 0, pal = "YlOrRd", n.colors = 9) {
-	# Breaking the numeric values into a factor
-	index <- cut(values, breaks = seq(min, max, length.out = n.colors))
-	palette <- RColorBrewer::brewer.pal(n = n.colors, name = pal)
-	output <- palette[as.integer(index)]
-	# Setting the values at the minimum to black
-	output[values <= min] <- "black"
-	output
-}
+# Setting some analysis parameters
+max_kmers <- 10000 # the maximum number of k-mers to map onto the haplotype sequences
+kmer_length <- 31 # the length of the k-mers used in the analysis
 
 # This script generates a k-mer plot from the consensus sequences of a group of samples
-locus <- commandArgs(trailingOnly = TRUE)[1]
 locus_data <- read.table("utilities/kmer_plot_ranges.txt")
 trait <- locus_data[locus_data[[1]] == locus, 2]
 
@@ -155,128 +147,13 @@ gaps <- str_locate_all(alignment, "-")
 gaps <- lapply(gaps, function(x) IRanges(start = x[, 1], end = x[, 2]))
 gaps <- lapply(gaps, function(x) reduce(x))
 
-# A function that takes a data.frame of plotting data and
-# a set of gaps, and adjusts the plotting positions accordingly
-adjust_gaps <- function(hapdata, gaps) {
-	if(!length(gaps)) return(hapdata)
-
-	for(i in 1:length(gaps)) {
-		hapdata[hapdata$pos >= start(gaps[i]), "pos"] <- hapdata[hapdata$pos >= start(gaps[i]), "pos"] + width(gaps[i])
-	}
-
-	return(hapdata)
-}
-
 plotting_data <- mapply(FUN = adjust_gaps, hapdata = plotting_data, gaps = gaps, SIMPLIFY = FALSE)
 
-# A function that fills the deleted positions with dashes
-fill_gaps <- function(x) {
-	# We need to get the set of positions covered from 1 to the maximum
-	maxpos <- max(do.call("rbind", x)$pos)
-
-	# Then we loop over all the data.frames and fill the missing positions with dashes
-	for(i in 1:length(x)) {
-		indices <- which(! 1:maxpos %in% x[[i]]$pos)
-		if(!length(indices)) next
-		new_rows <- data.frame(pos = indices,
-				       nuc = "-",
-				       log10p = 0,
-				       color = "black",
-				       stringsAsFactors = FALSE)
-		x[[i]] <- rbind(x[[i]], new_rows)
-		x[[i]] <- x[[i]][order(x[[i]]$pos), ]
-	}
-
-	x
-}
 
 plotting_data <- fill_gaps(plotting_data)
 
-# A function that finds the positions where the nucleotides differ between two consecutive sequences
-nucdiff <- function(hapdata) {
-	if(length(hapdata) <= 1) stop("nucdiff needs at least two haplotypes to compare")
-
-	# Creating a list that will contain the positions that differ
-	output <- list()
-
-	for(i in 1:(length(hapdata) - 1)) {
-		if(!all(hapdata[[i]]$pos == hapdata[[i + 1]]$pos)) stop("All positions must be shared between all haplotypes")
-
-		output[[i]] <- which(hapdata[[i]]$nuc != hapdata[[i + 1]]$nuc & hapdata[[i]]$nuc != "-" & hapdata[[i + 1]]$nuc != "-")
-	}
-
-	output
-}
 
 difflist <- nucdiff(plotting_data)
-
-# A function that sets up viewports for plotting haplotypes and draws them
-grid.haplotypes <- function(plotting_data, difflist, fontsize = 8) {
-
-	# Determining the number of columns as the maximum position to plot
-	ncolumns <- max(do.call("rbind", plotting_data)$pos)
-
-	# Dividing the viewport into rows
-	# Haplotype rows are interleaved with rows used to show the differences
-	#  between consecutive haplotypes, hence the number of rows being 2 * (number of haplotypes) - 1
-	grid::pushViewport(grid::viewport(layout = grid::grid.layout(nrow = length(plotting_data) * 2 - 1,
-								     ncol = ncolumns)))
-
-	# Plotting each haplotype in its row
-	for(i in 1:length(plotting_data)) {
-		hapdata <- plotting_data[[i]]
-
-		for(j in 1:nrow(hapdata)) {
-			grid.text(hapdata[j, "nuc"],
-				  gp = grid::gpar(fontsize = fontsize,
-						  fontfamily = "mono",
-						  fontface = ifelse(hapdata[j, "log10p"] != 0, "bold", "plain"),
-						  col = hapdata[j, "color"]),
-				  vp = grid::viewport(layout.pos.row = i * 2 - 1,
-						      layout.pos.col = hapdata[j, "pos"]))
-		}
-	}
-
-	# Also plotting the positions where the nucleotides differ
-	for(i in 1:length(difflist)) {
-		if(!length(difflist[[i]])) next
-
-		for(j in difflist[[i]]) {
-			grid::grid.lines(x = 0.5, y = c(0, 1), vp = grid::viewport(layout.pos.row = i * 2, layout.pos.col = j))
-		}
-	}
-
-	# Moving back into the top viewport
-	grid::upViewport()
-
-}
-
-grid.phenotable <- function(haplotype_data) {
-	# Computing a table of the phenotypes observed per character
-	phenotable <- table(haplotype_data$phenotype_character, haplotype_data$id)
-
-	# Creating a viewport with the required cells to print the data
-	grid::pushViewport(grid::viewport(layout = grid::grid.layout(nrow = nrow(phenotable) + 1,
-								     ncol = ncol(phenotable) + 1)))
-
-	# Printing the IDs of the phenotypes
-	for(i in 1:ncol(phenotable)) grid.text(colnames(phenotable)[i], vp = grid::viewport(layout.pos.row = 1, layout.pos.col = i + 1))
-
-	# Printing the phenotype values themselves
-	for(i in 1:nrow(phenotable)) grid.text(rownames(phenotable)[i], vp = grid::viewport(layout.pos.row = i + 1, layout.pos.col = 1))
-
-	# Printing the counts
-	for(i in 1:nrow(phenotable)) {
-		for(j in 1:ncol(phenotable)) {
-			grid.text(as.character(phenotable[i, j]), vp = grid::viewport(layout.pos.row = i + 1, layout.pos.col = j + 1))
-		}
-	}
-
-	# Returning to the top viewport
-	upViewport()
-
-	return(invisible(NULL))
-}
 
 # Outputting to a png file
 png(paste0("figures/", locus, "_kmers.png"), width = 8, height = 2, units = "in", res = 100)
