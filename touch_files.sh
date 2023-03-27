03f5a086e7f22e54b2b666211c33d161646e91b2
Marc-André Lemay <marc-andre.lemay.2@ulaval.ca>
2023-03-27 18:27:44 -0400

    Add some files to touch_files.sh

--- touch_files.sh
+++ touch_files.sh
@@ -7,6 +7,13 @@ touch refgenome/Gmax_508_Wm82.a4.v1.gene_exons.gff3
 touch reference_signals/bandillo2015_table1_curated.csv
 touch refgenome/Gmax_508_v4.0_mit_chlp.fasta.fai
 touch external_data/adapters.fa
+touch refgenome/Gmax4_db.bck
+touch refgenome/Gmax4_db.des
+touch refgenome/Gmax4_db.prj
+touch refgenome/Gmax4_db.sds
+touch refgenome/Gmax4_db.ssp
+touch refgenome/Gmax4_db.suf
+touch refgenome/Gmax4_db.tis
 
 mkdir -p illumina_data/raw_fastq
 
@@ -16,3 +23,41 @@ do
 	touch illumina_data/raw_fastq/${i}_2.fastq.gz
 done
 
+for i in $(cat external_data/nanopore_svs/files.txt)
+do
+	touch external_data/nanopore_svs/${i}
+done
+
+mkdir -p external_data/genome_assemblies/
+for i in $(cut -d " " -f1 variant_calling/assemblies/assembly_samples.txt)
+do
+	touch external_data/genome_assemblies/${i}
+	touch external_data/genome_assemblies/${i}.fai
+done
+
+for program in platypus kmers paragraph
+do
+	for i in $(cut -d, -f1 utilities/signal_ids.txt)
+	do
+		#touch gwas_results/${program}/${i}_locus.rds
+		touch gwas_results/${program}/${i}_top_markers.rds
+		touch gwas_results/${program}/${i}_all_genes.tsv
+		touch gwas_results/${program}/${i}_top_genes.tsv
+		touch gwas_results/${program}/${i}_nearest_gene.tsv
+	done
+done
+
+mkdir -p illumina_data/merged_bams/
+mkdir -p sv_genotyping/paragraph/manifest_files/
+
+for i in $(cut -d ' ' -f1 utilities/srr_id_correspondence.txt)
+do
+	touch illumina_data/merged_bams/${i}_merged.bam
+	touch illumina_data/merged_bams/${i}_merged.bam.bai
+	touch sv_genotyping/paragraph/manifest_files/${i}_manifest.txt
+done
+
+touch external_data/BAC77G7-a.fasta
+touch external_data/BAC77G7-a.fasta.fai
+touch refgenome/Gmax_nuclv2_mit_chlp.fasta
+touch external_data/soysnp50k_wm82.a2_41317.vcf.gz
